ea96e50692c7f529c22c2116a108de401c1ac405
gfs <gongfangsheng@zhongan.com>
2018-06-20 23:44:37 +0800

    大量修改

--- utils/api.js
+++ utils/api.js
@@ -1,10 +1,27 @@
-const HOST_URI = 'https://cate.ergengtv.com';
-const ACT_URI = 'https://act.ergengtv.com';
+// const HOST_URI = 'https://cate.ergengtv.com';
+const HOST_URI = 'https://api.test.yinxinlife.com';
 const VERSION = '0.3';
 
-const GET_USER_INFO = '/api/auth/wxapp/login';//获取注册用户TOKEN
-const GET_UPDATA_USER_INFO = '/api/auth/wxapp/update';//更新用户TOKEN
-const POST_USER_INFO = '/api/auth/wxapp/setUserInfo'
+const POST_USER_TICKET_LIST = '/user/ticketList'
+const POST_USER_EDIT = '/user/edit'
+const GET_USER_HOME = '/user/home'
+const GET_TICKET_DETAIL = '/ticket/detail'
+const POST_REQUIRE_CREATE = '/require/create'
+const GET_REQUIRE_DETAIL = '/require/detail'
+const GET_EXPERIENCE_ACTIVITY = '/experience/activity'
+const POST_USER_FORM_ID = '/search/homestay'
+const GET_SEARCH_HOMESTAY = '/search/homestay'
+const POST_FILE_UPLOAD = '/file/upload'
+const POST_EXPERIENCE_APPLY = '/experience/apply'
+const GET_EXPERIENCE_DETAIL = '/experience/detail'
+const GET_EXPERIENCE_HOME = '/experience/home'
+const GET_HOMESTAY_DETAIL = '/homestay/detail'
+const GET_HOUSE_DETAIL = '/house/detail'
+const POST_ACTIVITY_ADD_CHOP = '/activity/addChop'
+const POST_ACTIVITY_ADD_GROUP = '/activity/addGroup'
+const GET_HOME_HOME = '/home/home'
+const GET_ACTIVITY_CHOP = '/activity/chop'
+const GET_ACTIVITY_GROUP = '/activity/group'
 const SCROLL_TIME = 5000
 
 module.exports = {
@@ -21,18 +38,84 @@ module.exports = {
   getVersionNum() {
     return '0.0.1'
   },
-  updataToken() {
-    return HOST_URI + GET_UPDATA_USER_INFO
+  // 我的票券
+  postUserTicketList() {
+    return HOST_URI + POST_USER_TICKET_LIST
   },
-  // 获取用户信息
-  getUserInfo() {
-    return HOST_URI + GET_USER_INFO;
+  // 编辑个人信息
+  postUserEdit() {
+    return HOST_URI + POST_USER_EDIT
   },
-  // 提交用户信息
-  postUserInfo() {
-    return HOST_URI + POST_USER_INFO;
+  // 我的首页
+  getUserHome() {
+    return HOST_URI + GET_USER_HOME
   },
-  scrollTime(){
-    return SCROLL_TIME
+  // 票券详情
+  getTicketDetail() {
+    return HOST_URI + GET_TICKET_DETAIL
+  },
+  // 申请团建会议
+  postRequireCreate() {
+    return HOST_URI + POST_REQUIRE_CREATE
+  },
+  // 团建会议申请页
+  getRequireDetail() {
+    return HOST_URI + GET_REQUIRE_DETAIL
+  },
+  // 体验活动详情页
+  getExperienceActivity() {
+    return HOST_URI + GET_EXPERIENCE_ACTIVITY
+  },
+  // 收集用户FORMID
+  postUserFormId() {
+    return HOST_URI + POST_USER_FORM_ID
+  },
+  // 民宿搜索列表页
+  getSearchHomestay() {
+    return HOST_URI + GET_SEARCH_HOMESTAY
+  },
+  // 文件上传
+  postFileUpload() {
+    return HOST_URI + POST_FILE_UPLOAD
+  },
+  // 申请成为体验师
+  postExperienceApply() {
+    return HOST_URI + POST_EXPERIENCE_APPLY
+  },
+  // 体验申请页
+  getExperienceDetail() {
+    return HOST_URI + GET_EXPERIENCE_DETAIL
+  },
+  // 体验首页
+  getExperienceHome() {
+    return HOST_URI + GET_EXPERIENCE_HOME
+  },
+  // 民宿详情
+  getHomestayDetail() {
+    return HOST_URI + GET_HOMESTAY_DETAIL
+  },
+  // 房型详情页
+  getHouseDetail() {
+    return HOST_URI + GET_HOUSE_DETAIL
+  },
+  // 帮忙砍价
+  postActivityAddChop() {
+    return HOST_URI + POST_ACTIVITY_ADD_CHOP
+  },
+  // 加入拼团
+  postActivityAddGroup() {
+    return HOST_URI + POST_ACTIVITY_ADD_GROUP
+  },
+  // 首页
+  getHomeHome() {
+    return HOST_URI + GET_HOME_HOME
+  },
+  // 砍价活动详情页
+  getActivityChop() {
+    return HOST_URI + GET_ACTIVITY_CHOP
+  },
+  // 拼团活动详情页
+  getActivityGroup() {
+    return HOST_URI + GET_ACTIVITY_GROUP
   },
 };
